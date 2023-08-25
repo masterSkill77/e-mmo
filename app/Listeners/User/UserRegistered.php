a39de8c40c3b0d19914f0e23f313036238a44763
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-25 11:02:04 +0300

    feat : add confirmation email

---
+++ app/Listeners/User/UserRegistered.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners\User;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Events\User\UserRegistered as UserRegisteredEvent;
+use App\Mail\ConfirmationMail;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Mail;
+
+class UserRegistered
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserRegisteredEvent $event): void
+    {
+        $link = env("APP_URL") . "/api/verify-email/" . Crypt::encryptString($event->user->email);
+        Mail::to($event->user->email)->send(new ConfirmationMail($event->user, $link));
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Agence::class, 'responsable_id');
     }
+
+    protected $dispatchesEvents = [
+        'created' => \App\Events\User\UserRegistered::class,
+    ];
 }
