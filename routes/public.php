5757767885507c2e7b213ce6b72db9c7cc4c0640
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-28 17:15:03 +0300

    feat : register public routes

--- routes/public.php
+++ routes/public.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\API\EstateController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("/public")->group(function () {
+Route::prefix("v1/public")->group(function () {
     Route::prefix("/estate")->group(function () {
         Route::get("/", [EstateController::class, 'all']);
     });
