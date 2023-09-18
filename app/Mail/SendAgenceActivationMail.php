ddeb89b80336950fbc3e503200db511fb83c5195
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-09-18 16:23:59 +0300

    feat : add email notification for account activated

--- app/Mail/SendAgenceActivationMail.php
+++ app/Mail/SendAgenceActivationMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\Agence;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -16,7 +17,7 @@ class SendAgenceActivationMail extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(public Agence $agence)
     {
         //
     }
@@ -37,7 +38,7 @@ class SendAgenceActivationMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mail.agence-activated',
         );
     }
 
--- resources/views/mail/agence-activated.blade.php
+++ resources/views/mail/agence-activated.blade.php
@@ -1,5 +1,5 @@
 <div>
     Your agence <b>{{$agence->agence_name}}</b> has been successfully activated
     <hr style="width: 100%;height: 10px" />
-    Please, click the link belows to access to this agence : <a href="{{env('APP_URL') . '/' . $agence->id }}">See you agence</a>
+    Please, click the link belows to access to this agence : <a href="{{env('CLIENT_APP_URL') . '/agence' . $agence->id }}">See you agence</a>
 </div>
